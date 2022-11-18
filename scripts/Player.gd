fc8657e039b1ac1681fb2b2c4c99908a4867215b
Zakhar Voloshchuk (3elDU) <3elduu@gmail.com>
2022-11-19 00:38:29 +0200

    added platformer minigame - added portal to platformer scene and back

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -7,8 +7,11 @@ export var jump = 125
 var velocity = Vector2.ZERO
 
 func _physics_process(_delta):
-	# gravity force
+	# apply gravity force
 	velocity.y += gravity
+	# reset y velocity, if player is on ground
+	if is_on_floor():
+		velocity.y = 0
 	
 	# reset x velocity on each frame
 	velocity.x = 0
@@ -21,5 +24,13 @@ func _physics_process(_delta):
 	# jumping
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = -jump
+	
+	# check for collision with teleport (door)
+	for i in get_slide_count():
+		var collision = get_slide_collision(i)
+		if collision.collider.is_in_group("teleport-spawn"):
+			get_tree().change_scene("res://base-scene.tscn")
+		elif collision.collider.is_in_group("teleport-platformer"):
+			get_tree().change_scene("res://platformer-scene.tscn")
 		
 	move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
