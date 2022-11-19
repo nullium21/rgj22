c86acc32d0d38dac99fb7226d08d7e3e248818e3
Zakhar Voloshchuk (3elDU) <3elduu@gmail.com>
2022-11-19 21:38:34 +0200

    improved clouds

--- skybox/CloudSpawner.gd
+++ skybox/CloudSpawner.gd
@@ -8,7 +8,7 @@ var clouds_textures = [
 	load("res://skybox/cloud5.tres")
 ]
 
-export var cloud_count = 100
+export var cloud_count = 50
 var current_clouds = []
 
 func _init():
@@ -18,14 +18,17 @@ func _physics_process(_delta):
 	# loop over current clouds, and check if cloud is over the visible viewport
 	var new_clouds = []
 	for cloud in current_clouds:
-		if (cloud as Sprite).position.x < 180:
+		if (cloud as Sprite).position.x < 200:
 			new_clouds.append(cloud)
+		else:
+			# remove old cloud
+			(cloud as Node2D).queue_free()
 
 	current_clouds = new_clouds
 
 	# move clouds
 	for cloud in current_clouds:
-		(cloud as Sprite).position.x += 0.33
+		(cloud as Sprite).position.x += (cloud as Node2D).get_meta("speed", 0.33)
 
 	# spawn new clouds
 	if len(current_clouds) < cloud_count:
@@ -35,6 +38,9 @@ func _physics_process(_delta):
 			cloud.texture = clouds_textures[j]
 			cloud.position.x = -160 - cloud.texture.get_width() - rand_range(0, 300)
 			cloud.position.y = rand_range(-180, 180)
+			var scale = rand_range(1, 2.2)
+			cloud.scale = Vector2(scale, scale)
 			cloud.z_index = -998
+			cloud.set_meta("speed", rand_range(0.1, 0.5))
 			self.add_child(cloud)
 			current_clouds.append(cloud)
